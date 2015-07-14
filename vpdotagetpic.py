f3876077f00dc33e7daf946508392140909d3de3
Alan <missjily@qq.com>
2015-07-14 14:55:33 +0800

    version2

--- vpdotagetpic.py
+++ vpdotagetpic.py
@@ -1,9 +1,11 @@
-# !coding=utf-8
+#coding=utf-8
 
 import urllib2
 import urllib
 import re
 import HTMLParser
+import csv
+
 
 class site():
     def __init__(self,inputurl):
@@ -20,9 +22,47 @@ class site():
         pageinfo=self.getpage()
         patt=re.compile(pattern)
         contents=patt.findall(pageinfo)
-        print contents
+        with open('heros.csv','wb')as csvfile:
+            spamwriter=csv.writer(csvfile,dialect='excel')
+            for content in contents:
+                #urllib.urlretrieve(content[0],'%s.png'%content[1].decode('utf-8'))
+                spamwriter.writerow([content[0],content[1]])
+
+    def getheros(self):
+        pattern='(http://thumb.vpgamecdn.com/crop/59.*?.png)" width="59" height="33" alt="(.*?)"'
+        pageinfo=self.getpage()
+        patt=re.compile(pattern)
+        contents=patt.findall(pageinfo)
+        heros=[]
         for content in contents:
-            urllib.urlretrieve(content[0],'%s.png'%content[1].decode('utf-8'))
+            heros.append(content[1].decode('utf-8'))
+        return heros
+
+    def getheroinfo(self,hero):
+        herourl='http://dota2.vpgame.com/market/default.html?SteamItem[npc]=npc_dota_hero_%s&SteamItem[rarity]=&SteamItem[quality]=&SteamItem[bet]=&SteamItem[zh_name]=&lang=zh_cn'%hero
+        request=urllib2.Request(herourl)
+        response=urllib2.urlopen(request)
+        return response.read().decode('utf-8').encode('gbk')
+
+    def getsale(self,hero):
+        pattern=r'<span class="sell-c mr-10">(.*?)</span>(.*?)</p>\n<span class="">(.*?)</span>(.|\n)*?<div class="td">\n<span>(.*?)</span>'
+        info=self.getheroinfo(hero)
+        print info
+        patt=re.compile(pattern,re.S)
+        contents=re.findall(patt,info)
+        print contents
+        return contents
+
+    def start(self):
+        heros=self.getheros()
+        for hero in heros:
+            sales=self.getsale(hero)
+            filename='%s.csv'%hero
+            with open(filename,'wb')as csvfile:
+                spamwriter=csv.writer(csvfile,dialect='excel')
+                for sale in sales:
+                    spamwriter.writerow([sale[0],sale[1],sale[2],sale[4]])
+                break
     
 a=site('http://dota2.vpgame.com/market.html')
-a.getpic()
+a.start()
